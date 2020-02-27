8b6dea7895dbe38b19be0aca2e1c53a036cda94a
rex2630 <rex2630@gmail.com>
2020-02-27 14:30:01 +0100

    Update of the script

---
+++ locales/cs.lua
@@ -0,0 +1,18 @@
+Locales['cs'] = {
+	['valid_this_purchase'] = 'potvrdit?',
+	['yes'] = 'ano',
+	['no'] = 'ne',
+	['name_outfit'] = 'název outfitu?',
+	['not_enough_money'] = 'nemáš dostatek peněz',
+	['press_menu'] = 'stiskni ~INPUT_CONTEXT~ pro přístup do menu',
+	['clothes'] = 'oblečení',
+	['you_paid'] = 'zaplatil jsi ~g~$%s~s~',
+	['save_in_dressing'] = 'pojmenujes jej?',
+	['shop_clothes'] = 'obchod s oblečením',
+	['player_clothes'] = 'prevlekarna',
+	['shop_main_menu'] = 'vítejte!',
+	['saved_outfit'] = 'tvůj outfit byl uložen do tvé převlíkací místnosti. Děkujeme za návšťevu !',
+	['loaded_outfit'] = 'obnovil jsi outfit z tvé převlíkací místnosti. Děkujeme za návštěvu !',
+	['suppr_cloth'] = 'smazat',
+	['supprimed_cloth'] = 'tento outfit byl smazán z tvé převlíkací místnosti'
+}
